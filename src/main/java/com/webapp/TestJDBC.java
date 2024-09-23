ec6fb9b102da6771a27eca98f58235fc42cbd66e
shravyareddy7 <167501904+shravyareddy7@users.noreply.github.com>
2024-09-23 11:08:01 +0530

    Update TestJDBC.java

--- src/main/java/com/webapp/TestJDBC.java
+++ src/main/java/com/webapp/TestJDBC.java
@@ -1,19 +1,1 @@
-// package com.webapp;
 
-// import java.sql.Connection;
-// import java.sql.DriverManager;
-
-// public class TestJDBC {
-//     public static void main(String[] args) {
-//         String jdbcUrl="jdbc:mysql://localhost:3306/hibernate?useSSL=false&serverTimezone=UTC";
-//         String user="hibernate";
-//         String password="hibernate";
-//         try {
-//             System.out.println("Connecting to database"+jdbcUrl);
-//             Connection connection= DriverManager.getConnection(jdbcUrl,user,password);
-//             System.out.println("Connection successful");
-//         }catch (Exception e){
-//             e.printStackTrace();
-//         }
-//     }
-// }
