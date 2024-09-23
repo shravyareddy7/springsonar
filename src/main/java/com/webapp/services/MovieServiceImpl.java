79401ac2f33a60bab8e7581e0a52014bc75790c1
shravyareddy7 <167501904+shravyareddy7@users.noreply.github.com>
2024-09-23 13:01:12 +0530

    Update MovieServiceImpl.java

--- src/main/java/com/webapp/services/MovieServiceImpl.java
+++ src/main/java/com/webapp/services/MovieServiceImpl.java
@@ -2,7 +2,6 @@ package com.webapp.services;
 
 import com.webapp.dao.MovieDAO;
 import com.webapp.entities.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
